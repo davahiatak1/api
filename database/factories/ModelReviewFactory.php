678c1aa7d528ab9e21d60ab864edd64c004b018b
David Adorons-Dieu <dvdjesusvit@gmail.com>
2018-01-21 01:48:04 +0000

    Créations de model Product et Review avec php artisan make:model NomModel -a

---
+++ database/factories/ModelReviewFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Model\Review::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('products', 'ProductController');
+
+Route::prefix('products/{product}')->group(function (){
+	Route::apiResource('reviews', 'ReviewController');
+});
