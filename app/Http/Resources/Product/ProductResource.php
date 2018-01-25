66f0e2c863db13b5ba459bb5638e031da651b7dc
David Adorons-Dieu <dvdjesusvit@gmail.com>
2018-01-25 21:59:37 +0000

    ProductResource modifier

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends Resource
             "name"=> $this->name,
             "description"=> $this->detail,
             "price"=> $this->price,
-            "stock"=> $this->stock,
-            "discount"=> $this->discount,
+            "stock"=> $this->stock == 0 ? 'Pas de stock': $this->stock ,
+            "totalPrice"=> round((1 - ($this->discount/100))* $this->discount, 2),
+            "rating"=> $this->reviews->count() >0 ? round($this->reviews->sum('star')/$this->reviews->count(),2): 'Aucun Rating',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id),
+            ],
+            
         ];
     }
 }
